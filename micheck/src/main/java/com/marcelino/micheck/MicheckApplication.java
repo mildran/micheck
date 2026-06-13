3e76d4efaa38f9d252e7642defb28a1469612ccf
MARCELINO PEREZ LUNA <marcelino.perez.luna@gmail.com>
2026-06-13 18:19:46 +0200

    Añadir página de inicio y HomeController

---
+++ micheck/src/main/java/com/marcelino/micheck/MicheckApplication.java
@@ -0,0 +1,13 @@
+package com.marcelino.micheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MicheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicheckApplication.class, args);
+    }
+
+}
